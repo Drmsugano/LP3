c21eab67caa24010e86c3e1ea2060579fd0f54b9
Your Name <you@example.com>
2024-10-31 20:39:52 -0300

    Comeco de ORM

---
+++ JPAHibernate/src/main/java/br/edu/ifpr/bean/Teste.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.bean;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+public class Teste {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ JPAHibernate/src/main/java/br/edu/ifpr/sql/teste.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Aluno
+ * Created: 31 de out. de 2024
+ */
+
